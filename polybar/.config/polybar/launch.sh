0a02d0f6d95220a91e88d52bbb363a388f0f56f6
Consigliere <lauragv2005@gmail.com>
2025-09-15 13:12:04 +0200

    Fixed polybar directory

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using the default config location ~/.config/polybar/config.ini
+# Launch Polybar, using the default config location ~/.config/polybar/polybar/config.ini
 polybar top &
+polybar -c ~/.config/polybar/config.ini top &
 
 echo "Polybar launched..."
